39ed402cfb562915c54807a36f52999b38aba0e9
Leon <infoleonalfrancisco@gmail.com>
2025-12-29 23:11:00 -0600

    defined schema of team

--- base_response.py
+++ base_response.py
@@ -1,10 +1,19 @@
 from pydantic import BaseModel
 from typing import Optional,Any,List
+
+
 class BaseResponse(BaseModel):
     message:Optional[str] = None
     status_code:Optional[int] = None
     error:Optional[str] = None
     data:Optional[List[Any]] = None
+
+
+class Team(BaseModel):
+    id_team:Optional[int]
+    team_name:Optional[str]
+
+
 
 
 
