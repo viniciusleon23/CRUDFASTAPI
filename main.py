c0d38e94233d177ac25f778aa4df8b07984e1efd
Leon <infoleonalfrancisco@gmail.com>
2025-12-29 23:46:48 -0600

    logic for creating a player

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ from fastapi import FastAPI
 
 #endpoints
 from endpoints.healt_check import health_check
+from endpoints.create_player import create_players
+
+#respuestas
+from base_response import CreatePlayer
 
 app = FastAPI()
 
 @app.get("/api/v1/")
 def healt():
     return  health_check()
+
+
+@app.post("/api/v1/player")
+def player(player:CreatePlayer):
+    return create_players(player)
